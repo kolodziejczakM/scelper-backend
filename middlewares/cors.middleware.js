41818debdfe1cd1db84ddeab129526ed50804e1c
kolodziejczakM <kolodziejczak.mn@gmail.com>
2017-05-03 22:47:18 +0200

    Added DELETE method to CORS middleware

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -2,7 +2,7 @@
 exports.letLocalhost = (router) => {
     router.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-        res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, PATCH');
+        res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, PATCH, DELETE');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     });
